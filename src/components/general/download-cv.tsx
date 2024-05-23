a5104aea554e41d7db7582237d50e3a1ab142d46
Arya Kumar Johary <aryajohary@gmail.com>
2024-05-23 08:50:48 +0530

    cv

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/general/button';
 
 const DownloadCV = () => {
   return (
-    <Button onClick={() => window?.open('https://drive.google.com/file/d/1p9bjrO05Vnm-bN9mP-NXQhF8w481Yrm_/view?usp=sharing', '_blank')}>
+    <Button onClick={() => window?.open('https://drive.google.com/file/d/12eKMt7ZKmEv_WMBzvigoIn7UWM83o53s/view?usp=sharing', '_blank')}>
       Download CV
     </Button>
   );
