222e2be28475b5fab179d4cf56891872f7b8713e
Arya Kumar Johary <aryajohary@gmail.com>
2024-05-25 11:22:47 +0530

    chore: Update CV download link in DownloadCV component

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -2,9 +2,12 @@
 
 import Button from '@/components/general/button';
 
+
+const cvpath = '../../../public/files/AryaJohary.pdf'
+const gdrivecvpath = 'https://drive.google.com/file/d/1SKZGpEgUSkaa6npMcVqLhLf9QaMRjuaq/view?usp=sharing'
 const DownloadCV = () => {
   return (
-    <Button onClick={() => window?.open('../../../public/files/AryaJohary.pdf', '_blank')}>
+    <Button onClick={() => window?.open(gdrivecvpath, '_blank')}>
       Download CV
     </Button>
   );
