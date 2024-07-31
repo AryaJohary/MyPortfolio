fe15b88b25c96c89f9ad43ae7a678a691cfdea47
Arya Kumar Johary <aryajohary@gmail.com>
2024-07-31 11:26:32 +0530

    refactor: Update CV download link in DownloadCV component

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/general/button';
 
 
 const cvpath = '../../../public/files/Arya_Kumar_Johary.pdf'
-const gdrivecvpath = 'https://drive.google.com/file/d/1UVTq546W7PtbeOasiTVpSzYsUh7lZlHI/view?usp=sharing'
+const gdrivecvpath = 'https://drive.google.com/file/d/1xPu2bz8ADE3SVmStrYf7ZMVHY3s4VZ8p/view?usp=sharing'
 const DownloadCV = () => {
   return (
     <Button onClick={() => window?.open(gdrivecvpath, '_blank')}>
