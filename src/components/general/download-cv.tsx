493cc6615032a1062f43fece125c869644c6575a
Arya Kumar Johary <aryajohary@gmail.com>
2025-04-13 14:33:00 +0530

    cv

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/general/button';
 
 
 const cvpath = '../../../public/files/Arya_Kumar_Johary.pdf'
-const gdrivecvpath = 'https://drive.google.com/file/d/1CPrZjjxGS4vQ9TDRNNtz2AuB1TW3c9Cw/view?usp=sharing'
+const gdrivecvpath = 'https://drive.google.com/file/d/1hyrWafkEuyo2dsdE7k-Zx30IUAn4JWvC/view?usp=sharing'
 const DownloadCV = () => {
   return (
     <Button onClick={() => window?.open(gdrivecvpath, '_blank')}>
